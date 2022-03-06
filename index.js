0e4fe8780263a9924b5609e401be6f633a5d0b8d
Sariputa Tiong <info@digitalfuture.melbourne>
2022-03-06 17:14:35 +1100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+const pets = ["Milo", "Otis", "Garfield"]
+
+const append = pets.slice() //copy new pets array into append and add "Odie" to the end of array
+append.push("Odie")
+
+const prepend = pets.slice() //copy new pets array into prepend and add "Odie" to the start of array
+prepend.unshift("Odie")
+
+const removeLast = pets.slice()
+removeLast.pop()
+
+const removeFirst = pets.slice()
+removeFirst.shift()
